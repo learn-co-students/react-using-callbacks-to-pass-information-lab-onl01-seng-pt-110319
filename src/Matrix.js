9a8d4cb83d4883c80e4bee7ab01bdcf74799e343
kierra <51520037+kierrakay@users.noreply.github.com>
2020-08-01 11:24:37 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,10 +7,25 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    //below is the default state color of white for when a color is selected
+    this.state = {
+      selectedColor: '#FFF'
+    }
   }
 
+  //this method changes the state value with whatever is passed in as an argument
+  setSelectedColor = (newColor) => {
+    this.setState({
+      selectedColor: newColor
+    })
+  }
+
+
+  // Cell, on the other hand, only needs to know the currently selected 
+  // color, not change it. We can pass this in to every Cell returned by
+  //  genRow() as a prop called selectedColor.. as shown below:
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor} />)
   )
 
   genMatrix = () => (
@@ -20,7 +35,7 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector setSelectedColor={this.setSelectedColor} />
         <div id="matrix">
           {this.genMatrix()}
         </div>
@@ -32,3 +47,7 @@ export default class Matrix extends Component {
 Matrix.defaultProps = {
   values: learnSymbol
 }
+
+       /* As mentioned, ColorSelector is the interface where users will 
+        be able to select a color, so it will need access to setSelectedColor.
+         We can pass the needed function down as a prop: ... as shown line 38*/
